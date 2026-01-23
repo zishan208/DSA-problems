565a34cbbfddcb37209671ab98b9569ba06d16b6
zishan208 <zishan.work.601@gmail.com>
2026-01-23 15:34:23 +0530

    Time: 31 ms (16.7%), Space: 12.6 MB (94.01%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,69 +1,46 @@
-// class Solution {
-
-
-
-// private:
-
-// bool findAdjacent(string s){
-//     for(int i = 0; i < s.length()-1;i++){
-    
-//         if (s[i]==s[i+1])
-//         {
-//             return true;
-        
-           
-//         }
-        
-//     }
-//     return false;
-    
-// }
-// public:
-
-
-
-//     string removeDuplicates(string s) {
-//         while(findAdjacent(s)){
-
-//              for(int i = 0; i < s.length()-1;i++){
-    
-//         if (s[i]==s[i+1])
-//         {
-//            s.erase(i,2);
-//             if (i >= 2) i -= 2; // 
-//                     else i = -1;  
-           
-//         }
-        
-//     }
-
-
-          
-          
-            
-//         }
-// return s;
-        
-//     }
-// };
-
-
 class Solution {
-    public:
-
-string removeDuplicates(string s) {
-    string result;
-    for (char c : s) {
-        if (!result.empty() && result.back() == c) {
-            result.pop_back();  // remove duplicate
-        } else {
-            result.push_back(c);
+private:
+    bool findAdjacent(const string& s) {
+        for (int i = 0; i < (int)s.length() - 1; i++) {
+            if (s[i] == s[i + 1]) return true;
         }
+        return false;
     }
-    return result;
-}
 
+public:
+    string removeDuplicates(string s) {
+        while (findAdjacent(s)) {
+            for (int i = 0; i < (int)s.length() - 1; i++) {
+                if (s[i] == s[i + 1]) {
+                    s.erase(i, 2);
+                    if (s.empty()) break;   // guard against empty string
+                    if (i >= 2) i -= 2;     // step back safely
+                    else i = -1;            // restart from beginning
+                }
+            }
+        }
+        return s;
+    }
 };
+
+
+
+// class Solution {
+//     public:
+
+// string removeDuplicates(string s) {
+//     string result;
+//     for (char c : s) {
+//         if (!result.empty() && result.back() == c) {
+//             result.pop_back();  // remove duplicate
+//         } else {
+//             result.push_back(c);
+//         }
+//     }
+//     return result;
+// }
+
+// };
 
 
 
