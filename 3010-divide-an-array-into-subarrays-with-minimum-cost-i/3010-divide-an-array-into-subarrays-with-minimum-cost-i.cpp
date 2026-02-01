5a33f7865f0f9952ef43d9b1c4ea98efd6fd28a4
zishan208 <zishan.work.601@gmail.com>
2026-02-01 15:42:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        
+         int sum=0;
+
+        sort(nums.begin()+1,nums.end());
+   
+       sums=nums[0]+nums[1]+nums[2];
+       return sum ; 
+    }
+};
