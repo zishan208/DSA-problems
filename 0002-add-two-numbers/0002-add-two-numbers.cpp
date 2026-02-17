23b780324d193795b203e654758cbd23ef9bbc03
zishan208 <zishan.work.601@gmail.com>
2026-02-17 14:59:56 +0530

    Time: 3 ms (34.98%), Space: 77.3 MB (12.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,27 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0); // dummy head
-        ListNode* current = &dummy;
+private:
+    ListNode* reverses(ListNode* first) {
+        ListNode* current = first;
+        ListNode* prev = nullptr;
+        ListNode* forward = nullptr;
+
+        while (current != nullptr) {
+            forward = current->next;
+            current->next = prev;
+            prev = current;
+            current = forward;
+        }
+        return prev; // new head
+    }
+
+
+    ListNode* add(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
         int carry = 0;
 
-        while (l1 != NULL || l2 != NULL || carry != 0) {
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
             int sum = carry;
-            if (l1 != NULL) {
+            if (l1 != nullptr) {
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2 != NULL) {
+            if (l2 != nullptr) {
                 sum += l2->val;
                 l2 = l2->next;
             }
 
             carry = sum / 10;
-            current->next = new ListNode(sum % 10);
-            current = current->next;
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
         }
 
-        return dummy.next;
+        return dummy->next;
+    }
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // Step 1: Reverse both lists
+       // l1 = reverses(l1);
+        //l2 = reverses(l2);
+
+        // Step 2: Add them
+        ListNode* sum = add(l1, l2);
+
+        // Step 3: Reverse result to restore proper order
+        return sum;
     }
 };
-
