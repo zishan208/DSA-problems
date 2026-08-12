3f64b61e00b7574659071d8cae321ccb4b29e1e2
zishan208 <zishan.work.601@gmail.com>
2026-08-12 14:18:05 +0530

    Time: 12 ms (11.57%), Space: 28 MB (5.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,29 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+
+//         int range=nums.size()-1;
+//         map<int,bool> count;
+//         for (int i = 0; i < nums.size(); i++) {
+//             count[nums[i]]=true;
+//         }
+
+//         for (int i = 0; i <=nums.size(); i++) {
+//             if (!count[i]) {
+//                return i;
+//             } 
+//         }
+//         return 0;
+//     }
+// };
+
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-
-        int range=nums.size()-1;
-        map<int,bool> count;
-        for (int i = 0; i < nums.size(); i++) {
-            count[nums[i]]=true;
-        }
-
-        for (int i = 0; i <=nums.size(); i++) {
-            if (!count[i]) {
-               return i;
-            } 
+        unordered_set<int> seen(nums.begin(), nums.end());
+        for (int i = 0; i <= nums.size(); i++) {
+            if (!seen.count(i)) return i;
         }
         return 0;
     }
