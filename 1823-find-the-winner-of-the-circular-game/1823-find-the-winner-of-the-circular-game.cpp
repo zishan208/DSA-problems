52a099f78d16e4e487e14c9ec548acac1c2297fd
zishan208 <zishan.work.601@gmail.com>
2026-08-16 14:31:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (79.68%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,39 @@
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) {
+//         queue<int> q;
+//        for(int i=1;i<=n;i++){
+//         q.push(i);
+//        }
+//        int count=0;
+//       while(q.size()!=1){
+//         count++;
+//         if(count==k){
+//             count=0;
+//             q.pop();
+//         }else{
+//             int temp=q.front();
+//             q.pop();
+//             q.push(temp);
+
+//         }
+
+//       }
+
+//       return q.front();
+        
+
+//     }
+// };
+
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> q;
-       for(int i=1;i<=n;i++){
-        q.push(i);
-       }
-       int count=0;
-      while(q.size()!=1){
-        count++;
-        if(count==k){
-            count=0;
-            q.pop();
-        }else{
-            int temp=q.front();
-            q.pop();
-            q.push(temp);
-
+        int ans = 0; // Winner's index when n=1 (0-indexed)
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + k) % i;
         }
-
-      }
-
-      return q.front();
-        
-
+        return ans + 1; // Convert to 1-indexed
     }
 };
