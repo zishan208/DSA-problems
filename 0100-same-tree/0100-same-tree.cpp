7723b73601d963c05a20e66b116a4ab5cbcfbe55
zishan208 <zishan.work.601@gmail.com>
2026-08-17 11:40:15 +0530

    Time: 0 ms (100%), Space: 12.7 MB (78.45%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -10,15 +10,34 @@
  * };
  */
 
+// class Solution {
+//   public:
+//     bool isSameTree(TreeNode* r1, TreeNode* r2) {
+//         if (r1==nullptr && r2==nullptr) return true;
+//         if(r1==nullptr && r2!=nullptr)  return false;
+//          if(r1!=nullptr && r2==nullptr)  return false;
+//          if(r1->val!=r2->val) return false;
+//     return isSameTree(r1->left,r2->left)&isSameTree(r1->right,r2->right);
+     
+        
+//     }
+// };
+
 class Solution {
+    private:
+    bool finds(TreeNode* r1,TreeNode* r2){
+        if(r1==nullptr && r2== nullptr) return true;
+         if(r1==nullptr || r2== nullptr) return false;
+        
+        if(r1->val!=r2->val) return false;
+        return finds(r1->left,r2->left) && finds(r1->right,r2->right);
+        
+    }
   public:
     bool isSameTree(TreeNode* r1, TreeNode* r2) {
-        if (r1==nullptr && r2==nullptr) return true;
-        if(r1==nullptr && r2!=nullptr)  return false;
-         if(r1!=nullptr && r2==nullptr)  return false;
-         if(r1->val!=r2->val) return false;
-    return isSameTree(r1->left,r2->left)&isSameTree(r1->right,r2->right);
-     
+        
+        return finds(r1,r2);
+    
         
     }
 };
