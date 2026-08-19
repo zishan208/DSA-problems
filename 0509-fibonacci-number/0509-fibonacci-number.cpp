2c67ecf3c8db17efded3656379be9713387100f7
zishan208 <zishan.work.601@gmail.com>
2026-08-19 20:54:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,20 +1,40 @@
 class Solution {
-
-private:
-        int fibonacci(int n, vector<int> &p) {
-    if (n == 0 || n == 1) return n;
-    if (p[n] != -1) return p[n];
-
-    p[n] = fibonacci(n - 1, p) + fibonacci(n - 2, p);
-    return p[n];
-}
-
 public:
     int fib(int n) {
+if (n == 0) return 0;   
+        if (n == 1) return 1;   
 
-        vector<int> p(n+1,-1);
-        
-       
-        return fibonacci(n,p);
+        vector<int> fibb(n + 1);
+        fibb[0] = 0;
+     fibb[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibb[i] = fibb[i - 1] + fibb[i - 2];
+        }
+
+        return fibb[n];
     }
 };
+
+
+
+// class Solution {
+
+// private:
+//         int fibonacci(int n, vector<int> &p) {
+//     if (n == 0 || n == 1) return n;
+//     if (p[n] != -1) return p[n];
+
+//     p[n] = fibonacci(n - 1, p) + fibonacci(n - 2, p);
+//     return p[n];
+// }
+
+// public:
+//     int fib(int n) {
+
+//         vector<int> p(n+1,-1);
+        
+       
+//         return fibonacci(n,p);
+//     }
+// };
