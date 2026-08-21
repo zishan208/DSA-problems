a138ad01bc5077f9f8967123c76275cf714d57c5
zishan208 <zishan.work.601@gmail.com>
2026-08-21 22:12:59 +0530

    Time: 5 ms (16.18%), Space: 15.6 MB (52.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -4,32 +4,15 @@ private:
         if (count_ > n || _count > n) return;
 
         if (count_ == n && _count == n) {
-          stack<char> st;
-
          
-            for (int i = 0; i < temp.length(); i++) {
-                if (temp[i] == '(') {
-          st.push(temp[i]);
-                } else { 
-                  
-                    if (st.empty() || st.top() != '(') {
-                        return;
-                } else {
-                        st.pop();
-                    }
-                }
-            }
-
-            if (st.empty()) {
                 ans.push_back(temp);
-            }
-            return;
+         return;
         }
 
         if (count_ < n) {
    check(n, count_ + 1, _count, temp + '(', ans);
         }
-        if (_count < n) {
+        if (_count < count_) {
             check(n, count_, _count + 1, temp + ')', ans);
         }
     }
