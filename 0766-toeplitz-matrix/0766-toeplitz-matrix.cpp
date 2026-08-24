8d2f7e95691b30750b05738c3fe70fc690e1cd65
zishan208 <zishan.work.601@gmail.com>
2026-08-24 19:14:04 +0530

    Time: 0 ms (100%), Space: 20.9 MB (99.56%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,3 +1,44 @@
+// class Solution {
+// public:
+//     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+
+//         int row_size=matrix.size();
+//         int col_size=matrix[0].size();
+//         if(row_size==1 && col_size==1) return true;
+//         int last_index=row_size+col_size-1;
+//         vector<vector<int>> ans(last_index);
+
+//         for(int i=0;i<row_size;i++){
+//             for(int j=0;j<col_size;j++){
+//                 int index=i - j + col_size - 1;
+//                 ans[index].push_back(matrix[i][j]);
+                
+//             }
+//         }
+
+//        for(int i=0;i<ans.size();i++){
+            
+        
+//             for(int j=0;j<ans[i].size()-1;j++){
+//                 if(ans[i].size()==1) continue;
+//                 if(ans[i][j]!=ans[i][j+1]) return false;
+
+
+
+
+                
+                
+                
+//             }
+//         }
+//         return true;
+        
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
@@ -5,32 +46,19 @@ public:
         int row_size=matrix.size();
         int col_size=matrix[0].size();
         if(row_size==1 && col_size==1) return true;
-        int last_index=row_size+col_size-1;
-        vector<vector<int>> ans(last_index);
-
+        
         for(int i=0;i<row_size;i++){
             for(int j=0;j<col_size;j++){
-                int index=i - j + col_size - 1;
-                ans[index].push_back(matrix[i][j]);
-                
-            }
-        }
 
-       for(int i=0;i<ans.size();i++){
-            
-        
-            for(int j=0;j<ans[i].size()-1;j++){
-                if(ans[i].size()==1) continue;
-                if(ans[i][j]!=ans[i][j+1]) return false;
-
-
-
-
-                
+                if((i-1)>-1 && (j-1)>-1){
+                    if(matrix[i][j]!=matrix[i-1][j-1]) return false;
+                }
                 
                 
             }
         }
+
+     
         return true;
         
     }
