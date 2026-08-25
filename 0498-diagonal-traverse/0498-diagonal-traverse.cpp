ecd87c334733502de68321c1bd1cc05fe6ec4bdf
zishan208 <zishan.work.601@gmail.com>
2026-08-25 09:30:10 +0530

    Time: 16 ms (34.45%), Space: 27.3 MB (26.27%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        
+         int rows = mat.size();
+        int cols = mat[0].size();
+        int count=rows*cols;
+       
+        
+        vector<vector<int>>  ans(rows+cols-1);
+
+
+        for(int i=0;i<count;i++){
+
+
+            int r = i / cols;     
+         int c = i % cols;    
+            int diagIdx = r + c;   
+
+            ans[diagIdx].push_back(mat[r][c]);
+            
+        }
+        vector<int> fans;
+
+        for(int i=0;i<ans.size();i++){
+            if(i%2==0){
+                reverse(ans[i].begin(),ans[i].end());
+            }
+            fans.insert(fans.end(),ans[i].begin(),ans[i].end());
+        }
+     return fans;   
+    }
+};
