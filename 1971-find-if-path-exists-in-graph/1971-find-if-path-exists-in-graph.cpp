fe9f89acdd1a7c260f81ac7e3d57035a60f71d67
zishan208 <zishan.work.601@gmail.com>
2026-08-28 00:57:11 +0530

    Time: 251 ms (79.08%), Space: 281.7 MB (78.58%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,25 +1,10 @@
 class Solution {
-private:
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, int destination) {
-        if (node == destination) return true;
-
-        visited[node] = true;
-
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-    
-                if (dfs(neighbor, adj, visited, destination)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-    
-        vector<vector<int>> adj(n);
+
+
+           vector<vector<int>> adj(n);
+           vector<bool> visited(n, false);
 
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0];
@@ -28,8 +13,61 @@ public:
             adj[v].push_back(u);
         }
 
-        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(source);
+        visited[source]=true;
+    while(!q.empty()){
+        int front=q.front();
+        if(front==destination) return true;
+        q.pop();
 
-        return dfs(source, adj, visited, destination);
+        for(int c:adj[front]){
+            if(!visited[c]){
+                q.push(c);
+                visited[c]=true;
+            }
+        }
+    }
+        return false;
+        
     }
 };
+
+
+
+
+// class Solution {
+// private:
+//     bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, int destination) {
+//         if (node == destination) return true;
+
+//         visited[node] = true;
+
+//         for (int neighbor : adj[node]) {
+//             if (!visited[neighbor]) {
+    
+//                 if (dfs(neighbor, adj, visited, destination)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+// public:
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    
+//         vector<vector<int>> adj(n);
+
+//         for (int i = 0; i < edges.size(); i++) {
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+
+//         vector<bool> visited(n, false);
+
+//         return dfs(source, adj, visited, destination);
+//     }
+// };
