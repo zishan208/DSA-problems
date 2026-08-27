e53521e4cfa4c400827616f568440a5131270e76
zishan208 <zishan.work.601@gmail.com>
2026-08-27 21:34:16 +0530

    Time: 25 ms (71.74%), Space: 23.3 MB (28.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,19 +1,81 @@
+// class Solution {
+// public:
+//     int numIslands(vector<vector<char>>& grid) {
+//         int row = grid.size();
+//         int col = grid[0].size();
+        
+//         vector<vector<bool>> visited(row, vector<bool>(col, false));
+//         int count = 0;
+
+//         for (int i = 0; i < row; i++) {
+//      for (int j = 0; j < col; j++) {
+//       if (grid[i][j] == '1' && !visited[i][j]) {
+//           queue<pair<int, int>> q;
+                    
+//                     q.push({i, j});
+//                     visited[i][j] = true;
+
+//                     while (!q.empty()) {
+//                         auto it = q.front();
+//                        q.pop();
+
+//                         int x = it.first;
+//                         int y = it.second;
+//                         if (x - 1 >= 0 && grid[x - 1][y] == '1' && !visited[x - 1][y]) {
+//                             visited[x - 1][y] = true;
+//                          q.push({x - 1, y});
+//                         }
+//                         if (x + 1 < row && grid[x + 1][y] == '1' && !visited[x + 1][y]) {
+//                       visited[x + 1][y] = true;
+//                             q.push({x + 1, y});
+//                         }
+//                         if (y + 1 < col && grid[x][y + 1] == '1' && !visited[x][y + 1]) {
+//                       visited[x][y + 1] = true;
+//                             q.push({x, y + 1});
+//                         }
+//                         if (y - 1 >= 0 && grid[x][y - 1] == '1' && !visited[x][y - 1]) {
+//                             visited[x][y - 1] = true;
+//                             q.push({x, y - 1});
+//                         }
+//                     }
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
         
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
+      
         int count = 0;
 
         for (int i = 0; i < row; i++) {
      for (int j = 0; j < col; j++) {
-      if (grid[i][j] == '1' && !visited[i][j]) {
+      if (grid[i][j] == '1') {
           queue<pair<int, int>> q;
                     
                     q.push({i, j});
-                    visited[i][j] = true;
+                    grid[i][j]=0;
 
                     while (!q.empty()) {
                         auto it = q.front();
@@ -21,20 +83,20 @@ public:
 
                         int x = it.first;
                         int y = it.second;
-                        if (x - 1 >= 0 && grid[x - 1][y] == '1' && !visited[x - 1][y]) {
-                            visited[x - 1][y] = true;
+                        if (x - 1 >= 0 && grid[x - 1][y] == '1' ) {
+                            grid[x - 1][y] = '0';
                          q.push({x - 1, y});
                         }
-                        if (x + 1 < row && grid[x + 1][y] == '1' && !visited[x + 1][y]) {
-                      visited[x + 1][y] = true;
+                        if (x + 1 < row && grid[x + 1][y] == '1' ) {
+                      grid[x + 1][y] = '0';
                             q.push({x + 1, y});
                         }
-                        if (y + 1 < col && grid[x][y + 1] == '1' && !visited[x][y + 1]) {
-                      visited[x][y + 1] = true;
+                        if (y + 1 < col && grid[x][y + 1] == '1' ) {
+                      grid[x][y + 1] = '0';
                             q.push({x, y + 1});
                         }
-                        if (y - 1 >= 0 && grid[x][y - 1] == '1' && !visited[x][y - 1]) {
-                            visited[x][y - 1] = true;
+                        if (y - 1 >= 0 && grid[x][y - 1] == '1' ) {
+                            grid[x][y - 1] = '0';
                             q.push({x, y - 1});
                         }
                     }
