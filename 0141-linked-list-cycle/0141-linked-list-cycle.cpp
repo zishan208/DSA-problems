01f799786d212ed016f7eb98c4eccdb12dc33740
zishan208 <zishan.work.601@gmail.com>
2026-09-04 10:11:15 +0530

    Time: 8 ms (81.54%), Space: 11.8 MB (53.58%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -5,24 +5,54 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- */
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+
+//         if(head==nullptr || head->next==nullptr) return false;
+//         unordered_map<ListNode*,bool> v;
+//         ListNode* curr=head;
+     
+//         while(curr!=nullptr) {
+//             if(v[curr]) return true;
+
+//              v[curr]=true;
+//             curr=curr->next;
+           
+
+//         }
+//         return false;
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
 
         if(head==nullptr || head->next==nullptr) return false;
-        unordered_map<ListNode*,bool> v;
-        ListNode* curr=head;
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
      
-        while(curr!=nullptr) {
-            if(v[curr]) return true;
+        while(fast && fast->next ) {
+          
+            slow=slow->next;
+            fast=fast->next->next;
 
-             v[curr]=true;
-            curr=curr->next;
+          
+
+
+              if(slow==fast) return true;
+         
            
 
         }
         return false;
+       
         
     }
 };
