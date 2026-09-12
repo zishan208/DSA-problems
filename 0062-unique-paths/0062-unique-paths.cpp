cacb390510a85099d0a34af39fd5ae8b3933492c
zishan208 <zishan.work.601@gmail.com>
2026-09-12 12:27:47 +0530

    Time: 2 ms (21.02%), Space: 9.3 MB (53.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,36 +1,32 @@
 class Solution {
 private:
-        void calculation(int r1, int r2, int &count,int m,int n,vector<vector<int>>& dp){
+        int calculation(int i, int j,int m,int n,vector<vector<int>>& dp){
+            if(i>m || j>n) return 0;
+            if(i==m && j==n){
+                return 1;
+            }
 
-            if(r1==m && r2==n){
-                    count ++;
-                    return;
-            } 
-            if(r1>m || r2>n) return;
-                if (dp[r1][r2] != -1) {
-            count += dp[r1][r2];
-            return;
-                                        }
- int before = count;
-      
-            //go to right
-            calculation(r1,r2+1,count,m,n,dp);
-            //go to bottom
-            calculation(r1+1,r2,count,m,n,dp);
-            dp[r1][r2] = count - before;
 
+            if(dp[i][j]!=-1) return dp[i][j];
+
+
+            return dp[i][j]=calculation(i+1,j,m,n,dp)+calculation(i,j+1,m,n,dp);
+
+            
+
+
+   \
         }
         
 public:
     int uniquePaths(int m, int n) {
-            //these are initials
-        int r1=1;//right
-        int r2=1;//bottom
-        int count=0;
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
 
-        calculation(r1,r2,count,m,n,dp);
+        
+    
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return calculation(0,0,m-1,n-1,dp);
          
-            return count;     
+                 
     }
 };
