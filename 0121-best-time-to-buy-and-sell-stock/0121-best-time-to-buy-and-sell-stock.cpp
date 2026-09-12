342c31cea85bacdd2454a59758e5a99a62b6d4a4
zishan208 <zishan.work.601@gmail.com>
2026-09-12 16:15:15 +0530

    Time: 3 ms (27.45%), Space: 97.4 MB (26.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,76 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         if (n == 0) return 0;
+
+//         vector<int> maxi(n);        
+//         int temp = prices[n - 1];  
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             if (prices[i] > temp) {
+//                 temp = prices[i];
+//             }
+//             maxi[i] = temp;   
+//         }
+
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             ans = max(ans, maxi[i] - prices[i]);
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+// class Solution {
+//     private:
+//     void fs(vector<int>& prices,int index,int &maxi){
+//         if(index<0) return 0;
+   
+//         maxi=max(maxi,prices[index]-maxi);
+//         fd(prices,index--,maxi);
+
+    
+//     }
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         if (n == 0) return 0;
+
+//        int maxi=0;
+//        fs(prices,index,maxi);
+//        return maxi;   
+//     }
+// };
+
+
 class Solution {
+private:
+    void fs(vector<int>& prices, int index, int& minPrice, int& maxProfit) {
+        if (index == prices.size()) return;
+
+        if (prices[index] < minPrice) {
+            minPrice = prices[index];
+        } else {
+            maxProfit = max(maxProfit, prices[index] - minPrice);
+        }
+
+        fs(prices, index + 1, minPrice, maxProfit);
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         if (n == 0) return 0;
 
-        vector<int> maxi(n);        
-        int temp = prices[n - 1];  
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (prices[i] > temp) {
-                temp = prices[i];
-            }
-            maxi[i] = temp;   
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, maxi[i] - prices[i]);
-        }
-
-        return ans;
+        fs(prices, 0, minPrice, maxProfit);
+        return maxProfit;
     }
 };
