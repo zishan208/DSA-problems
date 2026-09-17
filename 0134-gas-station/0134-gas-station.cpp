1b934768e66b95adb99bb3214252ee4710b0d500
zishan208 <zishan.work.601@gmail.com>
2026-09-17 18:44:53 +0530

    Time: 3 ms (35.3%), Space: 112.7 MB (14.7%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,30 +1,32 @@
-
-
-
 class Solution {
-  public:
-    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
-       // ith station of gas[i] available
-       //cost means distamce travel from ith to ith+1
-       int net_petrol=0;
-       int start=0;
-       int kami=0;
-        int n=gas.size();
-       for (int i = 0; i < n; i++)
-       {
-                net_petrol+=gas[i]-cost[i];
-                if(net_petrol<0){
-                    start=i+1;
-                    kami+=net_petrol;
-                    net_petrol=0;
-                    
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
+
+                int remained=0;
+                int n=gas.size();
+                int start=0;
+                int t=0;
+
+                for(int i=0;i <n ;i++){
+                    remained+=gas[i];
+
+
+                    if(remained >=cost[i]){
+                        remained-=cost[i];
+
+                    }else{
+                        if(i+1<n){
+                        start=i+1;
+                        }
+                        remained=0;
+                    }
+                    t=t+gas[i]-cost[i];
 
                 }
-            
-       }
-      if(kami+net_petrol>=0) return start;
 
-      return -1;
+                return t>=0 ? start : -1;
+
         
     }
 };
