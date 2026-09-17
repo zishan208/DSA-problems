d6f339f11457fe70dceca108e717251c08d04250
zishan208 <zishan.work.601@gmail.com>
2026-09-17 14:23:50 +0530

    Time: 0 ms (100%), Space: 52.3 MB (57.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,56 @@
+// class Solution {
+// private:
+//     bool check(vector<int>& nums,  int index, vector<int>& dp) {
+//         if (index >=nums.size()-1) return true;
+//         if (dp[index] != -1) return dp[index];
+
+//         for (int i = 1; i <= nums[index]; i++) {
+
+//          int newIndex = index + i;
+//             if (newIndex < nums.size() && check(nums,newIndex, dp)) {
+//                 return dp[index] = true;
+//             }
+//         }
+//         return dp[index] = false;
+//     }
+
+
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+//         vector<int> dp(nums.size(), -1); 
+//         return check(nums, 0, dp);
+//     }
+// };
+
+
+
+
+
+
 class Solution {
-private:
-    bool check(vector<int>& nums,  int index, vector<int>& dp) {
-        if (index >=nums.size()-1) return true;
-        if (dp[index] != -1) return dp[index];
-
-        for (int i = 1; i <= nums[index]; i++) {
-
-         int newIndex = index + i;
-            if (newIndex < nums.size() && check(nums,newIndex, dp)) {
-                return dp[index] = true;
-            }
-        }
-        return dp[index] = false;
-    }
 
 
 public:
     bool canJump(vector<int>& nums) {
+
+
+        int n=nums.size();
+        int max_cover=0;
+        for(int i=0 ; i<n ;i++){
+            if (i > max_cover) return false;
+
+            if(max_cover>=n-1) return true;
+
+            max_cover= max(max_cover,i+nums[i]);
+
+
+            
+
+        }
+
+        return true;
         
-        vector<int> dp(nums.size(), -1); 
-        return check(nums, 0, dp);
+       
     }
 };
