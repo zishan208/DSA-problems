6fd6fa187afa8a83daeb2e4f4e814130435d44d0
zishan208 <zishan.work.601@gmail.com>
2026-09-18 09:55:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (39.84%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,37 +1,67 @@
-class Solution {
-public:
-    vector<int> partitionLabels(string s) {
+// class Solution {
+// public:
+//     vector<int> partitionLabels(string s) {
 
-        unordered_map<char,int> lasts;
-        int n=s.length();
+//         unordered_map<char,int> lasts;
+//         int n=s.length();
 
-        for (int i = 0; i < n; i++) {
+//         for (int i = 0; i < n; i++) {
 
-            lasts[s[i]]=i;
+//             lasts[s[i]]=i;
              
-        }
+//         }
 
-        int p=0;
-        int e=lasts[s[0]];
-        vector<int> ans;
+//         int p=0;
+//         int e=lasts[s[0]];
+//         vector<int> ans;
     
        
 
-        for (int i = 0; i < n; i++) {
+//         for (int i = 0; i < n; i++) {
 
-            if(i==e){
-                int answer=e-p+1;
-                ans.push_back(answer);
+//             if(i==e){
+//                 int answer=e-p+1;
+//                 ans.push_back(answer);
                 
-                p=i+1;
-                   if (i + 1 < n) e = lasts[s[i + 1]];
+//                 p=i+1;
+//                    if (i + 1 < n) e = lasts[s[i + 1]];
 
-            }else{
-                 e=max(e,lasts[s[i]]);
-            }       
+//             }else{
+//                  e=max(e,lasts[s[i]]);
+//             }       
+//         }
+
+// return ans;
+        
+//     }
+// };
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int last[26] = {0}; // har character ka last index
+        int n = s.size();
+
+        // Har character ka last occurrence store karo
+        for (int i = 0; i < n; i++) {
+            last[s[i] - 'a'] = i;
         }
 
-return ans;
-        
+        vector<int> ans;
+        int start = 0, end = 0;
+
+        for (int i = 0; i < n; i++) {
+            end = max(end, last[s[i] - 'a']); // current partition ka end expand karo
+            if (i == end) {                   // partition complete
+                ans.push_back(end - start + 1);
+                start = i + 1;                // next partition start
+            }
+        }
+        return ans;
     }
 };
