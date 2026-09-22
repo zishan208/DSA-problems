487f66906cedfa023144f8675a634a86c9b9672f
zishan208 <zishan.work.601@gmail.com>
2026-09-22 18:41:58 +0530

    Time: 3 ms (29.62%), Space: 16.4 MB (15.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,34 +1,28 @@
 class Solution {
-     private:
-    int s(vector<vector<int>> &grid,int &row,int &col,int i,int j,vector<vector<int>> &dp){
-
-          if(i>=row || j>=col) return 0;
-        if(i==row-1 && j==col-1) return grid[i][j];
-      
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int sum1=INT_MAX;
-        int sum2=INT_MAX;
-           if(i+1<row ) sum1=grid[i][j]+s(grid,row,col,i+1,j,dp);
-           if(j+1<col) sum2=grid[i][j]+s(grid,row,col,i,j+1,dp);
-
-           dp[i][j]=min(sum1,sum2);
-
-           return dp[i][j];
+private:
+    int f(vector<vector<int>>& grid, int& row, int& col, int i, int j, vector<vector<int>>& dp) {
+        if (i >= row || j >= col) return INT_MAX;
 
 
+        if (i == row - 1 && j == col - 1) return grid[i][j];
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int op1 = INT_MAX, op2 = INT_MAX;
+
+        if (i + 1 < row) op1 = f(grid, row, col, i + 1, j, dp);
+        
+        if (j + 1 < col) op2 = f(grid, row, col, i, j + 1, dp);
+
+        dp[i][j] = grid[i][j] + min(op1, op2);
+        return dp[i][j];
     }
+
 public:
     int minPathSum(vector<vector<int>>& grid) {
-
-         int row=grid.size();
-        int col=grid[0].size();
-
-        vector<vector<int>> dp(row,vector<int>(col,-1));
-
-
-        return s(grid,row,col,0,0,dp);
-        
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, -1));
+        return f(grid, row, col, 0, 0, dp);
     }
 };
-
