a66b9a2f499f876d7899fdbdadc9a5bd0ff6a9e1
zishan208 <zishan.work.601@gmail.com>
2026-09-26 10:16:06 +0530

    Time: 85 ms (46.73%), Space: 122.7 MB (66.67%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         unordered_map<string,string> m;
-        for(int i = 0; i < knowledge.size(); i++){
-            m[knowledge[i][0]] = knowledge[i][1];
-        }
+        for (const auto& kv : knowledge) {
+      m[kv[0]] = kv[1];
+         }
 
 
         string ans = "";
@@ -24,3 +24,30 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     string evaluate(string s, vector<vector<string>>& knowledge) {
+//         unordered_map<string, string> m;
+//         for (const auto& kv : knowledge) {
+//             m[kv[0]] = kv[1];
+//         }
+
+//         string ans;
+//         string key;
+//         for (char c : s) {
+//             if (c == '(') {
+//                 ans += key;
+//                 key.clear();
+//             } else if (c == ')') {
+//                 ans += (m.count(key) ? m[key] : "?");
+//                 key.clear();
+//             } else {
+//                 key += c;
+//             }
+//         }
+//         ans += key;
+//         return ans;
+//     }
+// };
